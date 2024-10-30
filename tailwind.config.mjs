b69948e180fc1608656a87f2390832ac4555a071
Rebecca <repa0001@stud.kea.dk>
2024-10-30 16:01:12 +0100

    merchandise

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -15,5 +16,6 @@ export default {
       },
     },
   },
+
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
 };
