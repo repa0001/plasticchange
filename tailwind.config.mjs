1d79c68410af6beba94deaa140f79e6371fb1b86
Rebecca <repa0001@stud.kea.dk>
2024-10-30 12:02:01 +0100

    mobilknapper_2

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "yellow-one": "#F3BD19",
+        "bright-yellow": "#FFED06", // Undgå mellemrum i navne
+        "almost-black": "#2C2D26",
+        "blue-one": "#43489B",
+        "dark-red": "#C72B25",
+        "burned-orange": "#DF5231",
+        "beige-one": "#EADDB7",
+        "soft-white": "#FDF6EC",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+};
